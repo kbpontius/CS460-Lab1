c527cf95e3bc4c9484dff4b8ff1ca50c938ca337
Kyle Pontius <kbpontius@xactware.com>
2016-02-04 21:04:32 -0700

    lab1-problem2-1: Cleared up address variable names to be more explicit. config-3n-1Mbps-1Mbps-100ms-100ms: Updated link speed and propagation delay.

--- lab1-problem2-1.py
+++ lab1-problem2-1.py
@@ -12,7 +12,8 @@ import random
 
 class DelayHandler(object):
     def receive_packet(self,packet):
-        print packet.source_address, packet.destination_address, Sim.scheduler.current_time(), packet.ident, packet.created, Sim.scheduler.current_time() - packet.created, packet.transmission_delay, packet.propagation_delay, packet.queueing_delay
+        if packet.ident == 999999:
+            print "Current Time: %f, Packet Id: %d, Creation Time: %f, Elapsed Time: %f, TD: %f, PD: %f, QD: %f" % (Sim.scheduler.current_time(), packet.ident, packet.created, Sim.scheduler.current_time() - packet.created, packet.transmission_delay, packet.propagation_delay, packet.queueing_delay)
 
 
 if __name__ == '__main__':
@@ -28,34 +29,27 @@ if __name__ == '__main__':
     n3 = net.get_node('n3')
 
     # setup forwarding entries
-    n1.add_forwarding_entry(address=n2.get_address('n1'),link=n1.links[0])
-    n2.add_forwarding_entry(address=n1.get_address('n2'),link=n2.links[0])
-    n2.add_forwarding_entry(address=n3.get_address('n2'),link=n2.links[1])
-    # n3.add_forwarding_entry(address=n2.get_address('n3'),link=n3.links[0])
+    a2 = n2.get_address('n1')
+    a1 = n1.get_address('n2')
+    a3 = n3.get_address('n2')
+
+    n1.add_forwarding_entry(address=a2, link=n1.links[0])
+    n2.add_forwarding_entry(address=a1,link=n2.links[0])
+    n2.add_forwarding_entry(address=a3,link=n2.links[1])
+    n3.add_forwarding_entry(address=a2,link=n3.links[0])
+
+    # if packet is going from n1 -> n3
+    n1.add_forwarding_entry(address=a3,link=n1.links[0])
 
     # setup app
     d = DelayHandler()
+    net.nodes['n1'].add_protocol(protocol="delay",handler=d)
     net.nodes['n2'].add_protocol(protocol="delay",handler=d)
+    net.nodes['n3'].add_protocol(protocol="delay",handler=d)
 
-    # send 1,000 packets
-    p = packet.Packet(destination_address=n3.get_address('n1'),ident=1,protocol='delay',length=1000)
-    Sim.scheduler.add(delay=0, event=p, handler=n1.send_packet)
-
-
-    # take the link down
-    # Sim.scheduler.add(delay=1, event=None, handler=n1.get_link('n2').down)
-
-    # send one packet
-    # p = packet.Packet(destination_address=n2.get_address('n1'),ident=1,protocol='delay',length=1000)
-    # Sim.scheduler.add(delay=0, event=p, handler=n1.send_packet)
-
-    # bring the link up
-    # Sim.scheduler.add(delay=2, event=None, handler=n1.get_link('n2').up)
-
-    # send one packet (and now it goes through)
-    # p = packet.Packet(destination_address=n2.get_address('n1'),ident=1,protocol='delay',length=1000)
-    # Sim.scheduler.add(delay=0, event=p, handler=n1.send_packet)
-
+    for i in range(0, 1000000):
+        p = packet.Packet(destination_address=a3,ident=1,protocol='delay',length=8000)
+        Sim.scheduler.add(delay=0, event=p, handler=n1.send_packet)
 
     # run the simulation
     Sim.scheduler.run()
